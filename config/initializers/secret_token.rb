3b89848765a8692826636e13479ded8a33691d5a
Bitmaker <Bitmaker@Bitmaker08.local>
2013-04-13 09:34:28 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Organizer::Application.config.secret_token = '83e5bb145988e3e1b62fc2c8d5ea4190507ffa962e591043dce272b93660f5bcc83cd76bb860d736cc0a1f9729b727b2e1eaedfbba346c1c4e72878e70eabff2'
